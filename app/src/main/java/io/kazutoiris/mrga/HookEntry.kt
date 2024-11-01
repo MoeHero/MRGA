b2840e8a3ee6173e82645a235b2f5e243bd53a9e
MoeHero <MoeHero@users.noreply.github.com>
2024-11-01 21:02:05 +0800

    [feat] support 1.24.1

--- app/src/main/java/io/kazutoiris/mrga/HookEntry.kt
+++ app/src/main/java/io/kazutoiris/mrga/HookEntry.kt
@@ -3,11 +3,13 @@ package io.kazutoiris.mrga
 import android.content.Context
 import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
 import com.highcapable.yukihookapi.hook.factory.configs
+import com.highcapable.yukihookapi.hook.factory.constructor
 import com.highcapable.yukihookapi.hook.factory.encase
 import com.highcapable.yukihookapi.hook.factory.field
 import com.highcapable.yukihookapi.hook.factory.method
 import com.highcapable.yukihookapi.hook.log.YLog
-import com.highcapable.yukihookapi.hook.type.android.ActivityClass
+import com.highcapable.yukihookapi.hook.type.defined.VagueType
+import com.highcapable.yukihookapi.hook.type.java.IntType
 import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
 
 @InjectYukiHookWithXposed
@@ -32,11 +34,17 @@ object HookEntry : IYukiHookXposedInit {
             }
 
             try {
-                "app.revanced.bilibili.patches.main.ApplicationDelegate".toClass().method {
-                    returnType = ActivityClass
-                }.hook {
-                    replaceTo(null)
-                }.onAllFailure { YLog.error(it.toString()) }
+                "kofua.X".toClass().apply {
+                    constructor {
+                        param(IntType, VagueType, VagueType)
+                    }.hook {
+                        before {
+                            if(args().first().int() == 0) {
+                                result = null
+                            }
+                        }
+                    }
+                }
             } catch (ignored: Exception) {
             }
 
@@ -67,13 +75,19 @@ object HookEntry : IYukiHookXposedInit {
                         } catch (ignored: Exception) {
                         }
 
+
                         try {
-                            "app.revanced.bilibili.patches.main.ApplicationDelegate".toClass()
-                                .method {
-                                    returnType = ActivityClass
+                            "kofua.X".toClass().apply {
+                                constructor {
+                                    param(IntType, VagueType, VagueType)
                                 }.hook {
-                                replaceTo(null)
-                            }.onAllFailure { YLog.error(it.toString()) }
+                                    before {
+                                        if(args().first().int() == 0) {
+                                            result = null
+                                        }
+                                    }
+                                }
+                            }
                         } catch (ignored: Exception) {
                         }
                     }
