26aeb59df4dca35445e1d69c459eccd26ad01c43
kazutoiris <78157415+kazutoiris@users.noreply.github.com>
2024-11-01 14:18:47 +0800

    [feat] support more version

--- app/src/main/java/io/kazutoiris/mrga/HookEntry.kt
+++ app/src/main/java/io/kazutoiris/mrga/HookEntry.kt
@@ -1,8 +1,10 @@
 package io.kazutoiris.mrga
 
+import android.content.Context
 import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
 import com.highcapable.yukihookapi.hook.factory.configs
 import com.highcapable.yukihookapi.hook.factory.encase
+import com.highcapable.yukihookapi.hook.factory.field
 import com.highcapable.yukihookapi.hook.factory.method
 import com.highcapable.yukihookapi.hook.log.YLog
 import com.highcapable.yukihookapi.hook.type.android.ActivityClass
@@ -16,21 +18,68 @@ object HookEntry : IYukiHookXposedInit {
 
     override fun onHook() = encase {
         loadApp {
-            "com.bilibili.lib.blkv.internal.sp.BatchedSpImpl".toClass().method {
-                name = "getBoolean"
-            }.hookAll {
-                before {
-                    if (args(0).string().startsWith("user_blocked")) {
-                        result = false
+            try {
+                "com.bilibili.lib.blkv.internal.sp.BatchedSpImpl".toClass().method {
+                    name = "getBoolean"
+                }.hookAll {
+                    before {
+                        if (args(0).string().startsWith("user_blocked")) {
+                            result = false
+                        }
+                    }
+                }.onAllFailure { YLog.error(it.toString()) }
+            } catch (ignored: Exception) {
+            }
+
+            try {
+                "app.revanced.bilibili.patches.main.ApplicationDelegate".toClass().method {
+                    returnType = ActivityClass
+                }.hook {
+                    replaceTo(null)
+                }.onAllFailure { YLog.error(it.toString()) }
+            } catch (ignored: Exception) {
+            }
+
+            try {
+                "android.app.ActivityThread".toClass().method {
+                    name = "performLaunchActivity"
+                }.hook {
+                    before {
+                        val app = instanceClass?.field {
+                            name = "mInitialApplication"
+                        }?.get(instance)?.cast<Context>()
+
+                        appClassLoader =
+                            app?.javaClass?.method {
+                                name = "getClassLoader"
+                            }?.get(app)?.invoke() as ClassLoader?
+
+                        try {
+                            "com.bilibili.lib.blkv.internal.sp.BatchedSpImpl".toClass().method {
+                                name = "getBoolean"
+                            }.hookAll {
+                                before {
+                                    if (args(0).string().startsWith("user_blocked")) {
+                                        result = false
+                                    }
+                                }
+                            }.onAllFailure { YLog.error(it.toString()) }
+                        } catch (ignored: Exception) {
+                        }
+
+                        try {
+                            "app.revanced.bilibili.patches.main.ApplicationDelegate".toClass()
+                                .method {
+                                    returnType = ActivityClass
+                                }.hook {
+                                replaceTo(null)
+                            }.onAllFailure { YLog.error(it.toString()) }
+                        } catch (ignored: Exception) {
+                        }
                     }
                 }
-            }.onAllFailure { YLog.error(it.toString()) }
-
-            "app.revanced.bilibili.patches.main.ApplicationDelegate".toClass().method {
-                returnType = ActivityClass
-            }.hook {
-                replaceTo(null)
-            }.onAllFailure { YLog.error(it.toString()) }
+            } catch (ignored: Exception) {
+            }
         }
     }
 }
